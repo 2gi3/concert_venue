27d84421f4d10e249f10ec66d8b4ac736bb2c2f0
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2023-03-06 02:35:21 +0000

    image tested in homepage

--- base-concert-venue/__tests__/ui/home.test.tsx
+++ base-concert-venue/__tests__/ui/home.test.tsx
@@ -1,9 +1,16 @@
-import {render, screen} from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
 
-test('Page has correct heading and image',()=>{
-    render(<Home />)
+test("Page has correct heading and image", () => {
+  render(<Home />);
 
-    const Heading = screen.getAllByRole('heading', {name: 'Welcome to Popular ConcertVenue'})
-     expect(Heading).toBeInTheDocument()
-})
+  const Heading = screen.getByRole("heading", {
+    name: "Welcome to Popular Concert Venue",
+  });
+  expect(Heading).toBeInTheDocument();
+
+  const image = screen.getByRole("img", {
+    name: "Concert goer with hands in the shape of a heart",
+  });
+  expect(image).toBeInTheDocument();
+});
